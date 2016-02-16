43c9bb0ab64bf0a661ad082fdc2cef3cb4fde79c
louiis <louis.chihara@gmail.com>
2016-02-16 13:59:33 -0600

    finished script

--- run_Analysis.R
+++ run_Analysis.R
@@ -8,8 +8,8 @@ download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfile
 unzip("data/wearabledata.zip", exdir = "./data/")
 
 # get the text files to read
-file_list_test <- list.files("./data/UCI HAR Dataset/test/", pattern = "*.txt", recursive = TRUE)
-file_list_train <- list.files("./data/UCI HAR Dataset/train/", pattern = "*.txt", recursive = TRUE)
+file_list_test <- list.files("./data/UCI HAR Dataset/test/", pattern = "*.txt")
+file_list_train <- list.files("./data/UCI HAR Dataset/train/", pattern = "*.txt")
 
 # inverse the order of the files
 file_list_test <- file_list_test[order(desc(file_list_test))]
@@ -21,10 +21,7 @@ for (file in file_list_test){
     # if the merged dataset doesn't exist, create it
     if (!exists("test")){
         test <- read.table(paste0("./data/UCI HAR Dataset/test/",file))
-    }
-    
-    # if the merged dataset does exist, append to it
-    if (exists("test")){
+    } else {
         temp_dataset <-read.table(paste0("./data/UCI HAR Dataset/test/",file))
         test<-cbind(test, temp_dataset)
         rm(temp_dataset)
@@ -37,10 +34,7 @@ for (file in file_list_train){
     # if the merged dataset doesn't exist, create it
     if (!exists("train")){
         train <- read.table(paste0("./data/UCI HAR Dataset/train/",file))
-    }
-    
-    # if the merged dataset does exist, append to it
-    if (exists("train")){
+    } else {
         temp_dataset <-read.table(paste0("./data/UCI HAR Dataset/train/",file))
         train<-cbind(train, temp_dataset)
         rm(temp_dataset)
@@ -53,8 +47,21 @@ merged <- rbind(test,train)
 # change the column names
 names(merged)[1] <- "activity"
 names(merged)[2] <- "subject"
-names(merged)[3:length(names(merged))] <-  sapply(1:(length(names(merged))-2),function(x) {paste0("obs",x)})
+
+# get the names of the observations
+features <- read.table(file = "data/UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+featuretitles <- features[,2]
+#names(merged)[3:length(names(merged))] <-  features[,2]
+#names(merged)
 
 # Q2. Extract the mean and standard deviation for each measurement
-obs <- select(merged,obs1:obs1714)
-meas.mean <- summarize(obs, mean)
+tokeepmean <- which(sapply(featuretitles,grepl,pattern="mean"))
+tokeepstd <- which(sapply(featuretitles,grepl,pattern="std"))
+
+tokeep <- c(1,2,tokeepmean+2 ,tokeepstd+2)
+tokeep <- tokeep[order(tokeep)]
+
+df <- select(merged,tokeep)
+head(df)
+
+# Q3. Using descriptive activity names in dataset
